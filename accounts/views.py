d0d480ac90363f78d01bbef1881ce058b74c5378
dnowak <dorota.iza.nowak@gmail.com>
2019-10-20 23:13:26 +0200

    accounts app added

---
+++ accounts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+
+from .forms import RegistrationForm
+from .tokens import account_activation_token
+def accounts_register(request):
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False #bylo false
+            # Save the User object
+            user.save()
+
+            # get current site
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            # create Message
+            message = render_to_string('accounts/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            # send activation link to the user
+            user.email_user(subject=subject, message=message)
+            return HttpResponse('registered succesfully and activateion sent')   
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'accounts/register.html',{'form': registerForm})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError,User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('login')
+    else:
+        return render(request, 'accounts/account_activation_invalid.html')
+
+
+
+
