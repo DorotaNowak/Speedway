7504aeb406b64a22a05e3e7b23ed015d5c9a93fb
kszawlis <k.szawlis@gmail.com>
2019-10-27 20:13:59 +0100

    uzupełniłem bazę danych

--- apps/fantasy_league/models.py
+++ apps/fantasy_league/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
-class Player(models.Model):
+
+
+class Player2(models.Model):
     first_name=models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     team=models.CharField(max_length=30)
     nationality=models.CharField(max_length=30)
-    photo=models.ImageField()
-
-
+    price=models.FloatField()
+class Player3(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    team=models.CharField(max_length=30)
+    nationality=models.CharField(max_length=30)
+    price=models.FloatField()
--- apps/fantasy_league/player_info/collect_stats.py
+++ apps/fantasy_league/player_info/collect_stats.py
@@ -1,15 +1,14 @@
 import sqlite3
 path=r'C:\Users\Karol\PycharmProjects\speedway_site\Speedway\db2.sqlite3'
-conn = sqlite3.connect(path)
+sqliteConnection = sqlite3.connect(path)
+cursor = sqliteConnection.cursor()
+
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
 
-con = sqlite3.connect(path)
-cursor = con.cursor()
-cursor.execute("SELECT * FROM fantasy_league_player")
 
-htmlSource = requests.get('https://speedwayekstraliga.pl/statystyki/lista-klasyfikacyjna/').text
+htmlSource = requests.get('https://speedwayekstraliga.pl/statystyki/lista-klasyfikacyjna/?k=2&y=2019').text
 soup = BeautifulSoup(htmlSource)
 players=soup.find_all('tr')
 for p in players:
@@ -17,5 +16,13 @@ for p in players:
     if len(names_info)>1:
         name=names_info[0].text
         surname=names_info[1].text
-        print(name)
-        print(surname)
+        stats=p.find_all('td')
+        team=stats[2].text
+        avg=stats[9].text
+        sqlite_insert_with_param = """INSERT INTO 'fantasy_league_player3'
+                              ( 'first_name', 'last_name', 'team', 'nationality','price') 
+                              VALUES (?, ?, ?, ?, ?);"""
+
+        data_tuple = ( name, surname,team,'PL',avg)
+        cursor.execute(sqlite_insert_with_param, data_tuple)
+        sqliteConnection.commit()
