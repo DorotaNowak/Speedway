b9a1d2e1925e8361c6892b4c24e3e2e123933aa0
dnowak <dorota.iza.nowak@gmail.com>
2019-12-12 12:11:02 +0100

    settings changed

--- speedway_site/settings.py
+++ speedway_site/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 LOGOUT_REDIRECT_URL = '/'
+STATIC_ROOT = '/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Here you tell django to look for a folder named 'assets'
