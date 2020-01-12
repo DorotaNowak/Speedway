6dcdfd8e0e9adbb88e621f634a7deb21494ac46c
dnowak <dorota.iza.nowak@gmail.com>
2020-01-12 17:01:23 +0100

    team league table added

--- apps/fantasy_league/urls.py
+++ apps/fantasy_league/urls.py
@@ -1,26 +1,24 @@
-from django.contrib import admin
-from django.conf.urls import url
 from django.urls import path
 from .views import home_response
 from .views import after_login_response
 from .views import index
-from django.urls import path
-from .views import view
-from .views import create
-from .views import leagues
+from .views import my_teams
+from .views import create_team
+from .views import create_league
 from .views import chosen_league
 from .views import join_to_league
 from .views import add_teams_to_league
+from .views import my_leagues
 
 urlpatterns = [
     path('', home_response, name='home'),
     path('accounts/profile/', after_login_response, name='test'),
     path('<int:id>', index, name='index'),
-    path('create/', create, name='create'),
-    path('view/', view, name='view'),
-    path('leagues/', leagues, name='leagues'),
+    path('create-team/', create_team, name='create'),
+    path('my-teams/', my_teams, name='my-teams-name'),
+    path('create-league/', create_league, name='leagues'),
     path('leagues/<int:id>', chosen_league, name='chosen_league'),
-    path('join_to_league/',join_to_league),
-    path('join_to_league/add_teams_to_league/',add_teams_to_league, name='add_teams_to_league'),
-
+    path('my-leagues', my_leagues),
+    path('join-to-league/', join_to_league),
+    path('join-to-league/add-teams-to-league/', add_teams_to_league, name='add-teams-to-league-name')
 ]
--- apps/fantasy_league/views.py
+++ apps/fantasy_league/views.py
@@ -2,9 +2,11 @@ from .models import Team, Player, League
 from .forms import CreateTeamForm, CreateLeagueForm, JoinLeagueForm, AddTeamToLeague
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from datetime import date
 import hashlib
+import sqlite3
 
 
 def home_response(request):
@@ -17,7 +19,12 @@ def after_login_response(request):
 
 
 @login_required
-def create(response):
+def my_leagues(request):
+    return render(request, 'my_leagues.html')
+
+
+@login_required
+def create_team(response):
     if response.method == "POST":
         form = CreateTeamForm(response.POST)
 
@@ -32,52 +39,12 @@ def create(response):
     else:
         form = CreateTeamForm()
 
-    return render(response, "create.html", {"form": form})
+    return render(response, "create_team.html", {"form": form})
 
 
 @login_required
-def join_to_league(response):
-    all_leagues = League.objects.all()  # return all leagues
-    if response.method == "POST":
-        form = JoinLeagueForm(response.POST)
-
-        if form.is_valid():
-            print("valid")
-            name = form.cleaned_data["name"]
-            password = form.cleaned_data["password"]
-            all_leagues = [a.name for a in all_leagues]
-            if name not in all_leagues:
-                return render(response, "join_to_league.html")
-            h = hashlib.new('ripemd160')
-            h.update(bytes(password, encoding='utf-8'))
-
-            if h.hexdigest() == League.objects.get(name=name).password:
-                return HttpResponseRedirect("add_teams_to_league")
-
-        else:
-            return render(response, "join_to_league.html")
-    else:
-        form = JoinLeagueForm()
-    return render(response, 'join_to_league.html', {"all_leagues": all_leagues, "form": form})
-
-
-@login_required
-def add_teams_to_league(response):
-
-    if response.method == "POST":
-        user_teams=response.user.team.all()
-        user_team_names=[team.name for team in user_teams]
-        form = AddTeamToLeague(response.POST)
-        if form.is_valid():
-            name = form.cleaned_data["name"]
-            if name in user_team_names:
-                return HttpResponseRedirect("add_teams_to_league.html")
-        else:
-            return HttpResponseRedirect("add_team_to_league.html")
-    else:
-        print('cccc')
-        form = AddTeamToLeague()
-    return render(response, "add_team_to_league.html", {"form": form})
+def my_teams(request):
+    return render(request, "my_teams.html")
 
 
 @login_required
@@ -160,12 +127,59 @@ def index(response, id):
 
 
 @login_required
-def view(request):
-    return render(request, "view.html")
+def join_to_league(response):
+    all_leagues = League.objects.all()  # return all leagues
+    if response.method == "POST":
+        form = JoinLeagueForm(response.POST)
+
+        if form.is_valid():
+            print("valid")
+            name = form.cleaned_data["name"]
+            password = form.cleaned_data["password"]
+            all_leagues = [a.name for a in all_leagues]
+            if name not in all_leagues:
+                return render(response, "join_to_league.html")
+            h = hashlib.new('ripemd160')
+            h.update(bytes(password, encoding='utf-8'))
+
+            if h.hexdigest() == League.objects.get(name=name).password:
+                league_id=League.objects.get(name=name).id
+                print(league_id)
+                return HttpResponseRedirect(reverse("add-teams-to-league-name"), {"league_id":league_id})
+
+        else:
+            return render(response, "join_to_league.html")
+    else:
+        form = JoinLeagueForm()
+    return render(response, 'join_to_league.html', {"all_leagues": all_leagues, "form": form})
 
 
 @login_required
-def leagues(response):
+def add_teams_to_league(response):
+    if response.method == "POST":
+        user_teams = response.user.team.all()
+        user_team_names = [team.name for team in user_teams]
+        form = AddTeamToLeague(response.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            if name in user_team_names:
+                #print(league_id)
+                data=(Team.objects.get(name=name).id,20)
+                path = r'C:\Users\Dorota Nowak\Desktop\Speedway\db2.sqlite3'
+                sqliteConnection = sqlite3.connect(path)
+                cursor = sqliteConnection.cursor()
+                cursor.execute('insert into team_league values (?,?)', data)
+                sqliteConnection.commit()
+                return HttpResponseRedirect(reverse("add-teams-to-league-name"))
+        else:
+            return HttpResponseRedirect(reverse("add-teams-to-league-name"))
+    else:
+        form = AddTeamToLeague()
+    return render(response, "add_team_to_league.html", {"form": form})
+
+
+@login_required
+def create_league(response):
     if response.method == "POST":
         form = CreateLeagueForm(response.POST)
 
@@ -180,14 +194,14 @@ def leagues(response):
 
                 league = League(name=name, password=h.hexdigest())
                 league.save()
-                return HttpResponseRedirect("add_teams_to_league" % league.id)
+                return HttpResponseRedirect(reverse('test'))
             else:
-                return render(response, 'leagues.html', {"form": form})
+                return render(response, 'create_league.html', {"form": form})
 
     else:
         form = CreateLeagueForm()
 
-    return render(response, "leagues.html", {"form": form})
+    return render(response, "create_league.html", {"form": form})
 
 
 @login_required
